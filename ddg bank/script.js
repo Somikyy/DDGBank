cfad2aba0a3a99fe553ede73f1a6aa6428ef27cc
Somikyy <155802624+Somikyy@users.noreply.github.com>
2025-02-04 11:44:59 +0100

    Add files via upload

---
+++ ddg bank/script.js
@@ -0,0 +1,5 @@
+// Example script to show an alert when the form is submitted (you can add more functionality here)
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Merci pour votre message !");
+});
